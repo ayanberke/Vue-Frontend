1930d405f3c6447d74af893fca3062273884a99a
berke.ayan <82400213+SeeMeInCrown@users.noreply.github.com>
2022-04-04 23:05:43 +0300

    added footer

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
     <div id="home-page">
-      <nav id="nav-bar" class="navbar navbar-expand-lg navbar-light bg-light">
+      <nav
+        id="nav-bar"
+        class="navbar navbar-expand-lg sticky-top navbar-light bg-white"
+      >
         <div class="container">
-          <a href="#" class="navbar-brand"><img id="icon" src="../assets/logo.png"></a>
+          <a href="#" class="navbar-brand"
+            ><img id="icon" src="../assets/logo.png"
+          /></a>
           <button
             type="button"
             class="navbar-toggler"
@@ -15,16 +20,17 @@
           <div class="collapse navbar-collapse" id="navbarCollapse">
             <div class="navbar-nav">
               <a href="#" class="nav-item nav-link active"> <b>Home</b> </a>
-              <a href="#" class="nav-item nav-link"> <b>About</b> </a>
-              <a href="#" class="nav-item nav-link"> <b>Contact</b> </a>
+              <a href="#" class="nav-item nav-link"> <b>Conferences</b> </a>
+              <a href="#" class="nav-item nav-link"> <b>About Us</b> </a>
             </div>
           </div>
         </div>
       </nav>
 
       <h1 class="welcome-msg">
-        <h1>OPTIMIZE YOUR SCHEDULE</h1>
-        <h1>WITH CONFY</h1> <br>
+        <h1>OPTIMIZE YOUR SCHEDULE WITH</h1>
+        <h1>CONFY</h1>
+        <br />
         <h1>WE VALUE YOUR TIME</h1>
       </h1>
 
@@ -43,10 +49,10 @@
                   <div class="col-lg-3 col-md-3 col-sm-12 p-0">
                     <textarea placeholder="Enter Presenter "></textarea>
                   </div>
-                     <label for="date"></label> 
-                     <input type="date" id="date" name="date" /> 
+                  <label for="date"></label>
+                  <input type="date" id="date" name="date" />
                   <div class="col-lg-3 col-md-3 col-sm-12 p-0">
-                    <button type="button" class="btn btn-danger wrn-btn">
+                    <button type="button" class="btn btn-warning wrn-btn btn-lg">
                       Schedule
                     </button>
                   </div>
@@ -56,6 +62,38 @@
           </form>
         </div>
       </section>
+      <footer class="bg-light text-center text-lg-start">
+        <div class="container p-4">
+          <div class="row">
+            <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+              <h5 class="text-uppercase">Information</h5>
+              <p>
+                This is a web-based application that will schedule conferences using optimization. 
+                The application finds the most suitable conference program by optimization. 
+                Users schedule conferences and can view the scheduling result as a table.
+              </p>
+            </div>
+            <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+              <h5 class="text-uppercase">Mission</h5>
+
+              <p>
+                The purpose of the system is to solve problems that organizers face when planning their conferences more quickly and conveniently. 
+                The organizers will collect the information from the presenters and plan the day, time-slots and rooms of the conference with the help of the system. 
+                The possibility of conflict and loss of time will be prevented.
+
+
+              </p>
+            </div>
+          </div>
+        </div>
+        <div
+          class="text-center p-3"
+          style="background-color: rgba(0, 0, 0, 0.2)"
+        >
+          © 2022 Copyright:
+          <a class="text-dark" href="https://mdbootstrap.com/">confy.com</a>
+        </div>
+      </footer>
     </div>
   </div>
 </template>
@@ -66,43 +104,39 @@ export default {};
 
 <style scoped>
 #home-page {
-  background-color: #0a090a;
   background: url("../assets/conference.jpg") no-repeat center center;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
+  -moz-background-size: 100% 100%;           /* Firefox 3.6 */
+	-o-background-size: 100% 100%;           /* Opera 9.5 */
+	-webkit-background-size: 100% 100%;           /* Safari 3.0, Chrome */
+	background-size: 100% 100%;   
   min-height: 100vh;
   opacity: 0.9;
 }
 #nav-bar {
-  opacity: 0.8;
-
+  opacity: 0.9;
 }
-#icon{
-    margin-left: 3px;
-    display: block;
-    max-width: 100px;
-    opacity:1;
+#icon {
+  margin-left: 3px;
+  display: block;
+  max-width: 110px;
+  opacity: 1;
 }
 
 .search-sec div {
   margin: 1rem;
-  padding: 1rem;
+  position:relative;
   display: inline-block;
 }
 
-.welcome-msg{
-  @import url('https://fonts.googleapis.com/css2?family=Kanit:ital@1&display=swap');
-  margin: 150px 100px;
-  color:rgb(24, 198, 82);
+.welcome-msg {
+  @import url('https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap');
+  font-family: 'Permanent Marker', cursive;  margin: 150px 100px;
+  color: rgb(232, 164, 16);
   text-decoration: none;
   text-transform: uppercase;
-  text-shadow: 1px 1px 2px rgb(7, 7, 5), 0 0 25px rgb(15, 14, 10), 0 0 5px rgb(16, 15, 12);
-  font-family: 'Kanit', sans-serif;
+  text-shadow: 1px 1px 2px rgb(7, 7, 5), 0 0 25px rgb(15, 14, 10),
+    0 0 5px rgb(16, 15, 12);
   font-size: 50px;
-  
-  
 }
 textarea {
   border-radius: 0.5rem;
@@ -116,22 +150,11 @@ textarea {
   display: flex;
   font-size: 1rem;
 }
-button {
+
+label {
   border-radius: 0.5rem;
   border-collapse: collapse;
-  border-style: #000 1px solid;
-  resize: none;
-  margin: 0.5rem;
-  padding: 0.3rem;
-  width: 15rem;
-  justify-content: center;
-  display: flex;
-  font-size: 1rem;
-}
-label{
-  border-radius: 0.5rem;
-  border-collapse: collapse;
-  border-style: #000 1px solid;
+  border-style: rgb(217, 239, 12) 1px solid;
   resize: none;
   margin: 0.5rem;
   padding: 0.3rem;
